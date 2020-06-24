c99e344aad124556f76e798c57dd27530cab73e7
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2020-06-24 16:21:04 -0400

    Camera fragment setup

---
+++ app/src/main/java/com/leap_android/CameraFragment.java
@@ -0,0 +1,19 @@
+package com.leap_android;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class CameraFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.camera_fragment, container, false);
+    }
+
+
+}
