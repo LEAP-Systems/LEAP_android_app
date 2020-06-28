8a539ad94084578a339fab1c69ba6ee7d4ff3a2a
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2020-06-28 18:29:49 -0400

    Setting up general canvas of camera preview

--- app/src/main/java/com/leap_android/CameraFragment.java
+++ app/src/main/java/com/leap_android/CameraFragment.java
@@ -1,28 +1,60 @@
 package com.leap_android;
 
 import android.graphics.Camera;
+import android.hardware.camera2.CameraDevice;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.hardware.camera2.CameraDevice;
 import androidx.fragment.app.Fragment;
 
-public class CameraFragment extends Fragment {
+public class CameraFragment extends Fragment{
 
 
-    Camera mCamera;
-    int mNumberOfCameras;
-    int cameraId;
-    int rotation;
+    TextView testView;
+
+    CameraDevice camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    private final String tag = "VideoServer";
+
+    Button capture;
+    Boolean onOffCam = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View cam_view = inflater.inflate(R.layout.cam_frag_layout, container, false);
+
+        capture = cam_view.findViewById(R.id.btn_capture);
+        capture.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View arg0) {
+                if (onOffCam == false){
+
+                    onOffCam = true;
+                }
+                else{
+
+                    onOffCam = false;
+                }
+            }
+        });
+
+
+
+
         return cam_view;
     }
+
+
+
 
 
 
